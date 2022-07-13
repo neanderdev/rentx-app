7ea67bf237de7769d5d0b6ff9eac915524fe097e
neanderdev <neander.faria@hotmail.com>
2022-07-13 15:24:11 -0300

    Continuing the Details interface

--- src/screens/CarDetail/index.tsx
+++ src/screens/CarDetail/index.tsx
@@ -4,6 +4,15 @@ import {
     Container,
     Header,
     CarImage,
+    Content,
+    Details,
+    Description,
+    Brand,
+    Name,
+    Rent,
+    Period,
+    Price,
+    About,
 } from './styles';
 
 import { BackButton } from '../../components/BackButton';
@@ -21,6 +30,27 @@ export function CarDetail() {
                     imagesUrl={["https://purepng.com/public/uploads/large/purepng.com-audi-r8audicars-961524670148fabn4.png"]}
                 />
             </CarImage>
+
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Audi</Brand>
+
+                        <Name>R8</Name>
+                    </Description>
+
+                    <Rent>
+                        <Period>Ao dia</Period>
+
+                        <Price>R$ 120</Price>
+                    </Rent>
+                </Details>
+
+                <About>
+                    Este é automóvel desportivo. Surgiu do lendário touro de lide indultado
+                    na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+                </About>
+            </Content>
         </Container>
     );
 }
