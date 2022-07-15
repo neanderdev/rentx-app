731a1a7c76675e572a6dca387f16d63ffc270103
neanderdev <neander.faria@hotmail.com>
2022-07-15 10:48:05 -0300

    Usability on the button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -9,11 +9,17 @@ import {
 interface ButtonProps extends RectButtonProps {
     title: string;
     color?: string;
+    enabled?: boolean;
 }
 
-export function Button({ title, color, ...rest }: ButtonProps) {
+export function Button({ title, color, enabled = true, ...rest }: ButtonProps) {
     return (
-        <Container color={color} {...rest}>
+        <Container
+            color={color}
+            enabled={enabled}
+            style={{ opacity: enabled ? 1 : .5 }}
+            {...rest}
+        >
             <Title>{title}</Title>
         </Container>
     );
--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Alert, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useTheme } from 'styled-components';
 import { NavigationProp, ParamListBase, useNavigation, useRoute } from '@react-navigation/native';
 import { format } from 'date-fns';
@@ -52,14 +52,10 @@ export function Scheduling() {
     const [rentalPeriod, setRentalPeriod] = useState<RentalPeriod>({} as RentalPeriod);
 
     function handleSchedulingDetails() {
-        if (!rentalPeriod.startFormatted || !rentalPeriod.endFormatted) {
-            Alert.alert('Seleciona o intervalo para alugar.');
-        } else {
-            navigation.navigate('SchedulingDetails', {
-                car,
-                dates: Object.keys(markedDates),
-            });
-        }
+        navigation.navigate('SchedulingDetails', {
+            car,
+            dates: Object.keys(markedDates),
+        });
     }
 
     function handleBack() {
@@ -148,6 +144,7 @@ export function Scheduling() {
                 <Button
                     title="Confirmar"
                     onPress={handleSchedulingDetails}
+                    enabled={!!rentalPeriod.startFormatted}
                 />
             </Footer>
         </Container>
