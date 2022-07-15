bdebddba87cec2ad5f1cdb24d540cda2dd939663
neanderdev <neander.faria@hotmail.com>
2022-07-15 15:52:25 -0300

    Splash screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,4 +1,7 @@
+import { Inter_100Thin } from '@expo-google-fonts/inter';
 import React from 'react';
+import { Button, StyleSheet } from 'react-native';
+import Animated from 'react-native-reanimated';
 
 import {
     Container
@@ -7,7 +10,17 @@ import {
 export function Splash() {
     return (
         <Container>
+            <Animated.View style={styles.box} />
 
+            <Button title='Mover' onPress={() => { }} />
         </Container>
     );
 }
+
+const styles = StyleSheet.create({
+    box: {
+        width: 100,
+        height: Inter_100Thin,
+        backgroundColor: 'red',
+    }
+});
