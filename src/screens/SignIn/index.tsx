8d976a9bb7fcff910adc884460d2881df370e108
neanderdev <neander.faria@hotmail.com>
2022-07-19 13:02:36 -0300

    password input

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,6 +4,7 @@ import { useTheme } from 'styled-components';
 
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
+import { PasswordInput } from '../../components/PasswordInput';
 
 import {
     Container,
@@ -44,6 +45,11 @@ export function SignIn() {
                     keyboardType='email-address'
                     autoCorrect={false}
                     autoCapitalize='none'
+                />
+
+                <PasswordInput
+                    iconName='lock'
+                    placeholder='Senha'
                 />
             </Form>
 
