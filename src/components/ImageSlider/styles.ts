4cb921e8236e3d19e6a1810b882d6cf31a75fc74
neanderdev <neander.faria@hotmail.com>
2022-07-20 16:01:36 -0300

    First Step

--- src/components/ImageSlider/styles.ts
+++ src/components/ImageSlider/styles.ts
@@ -1,10 +1,6 @@
 import { Dimensions } from "react-native";
 import styled from "styled-components/native";
 
-interface ImageIndexProps {
-  active: boolean;
-}
-
 export const Container = styled.View`
   width: 100%;
 `;
@@ -14,17 +10,6 @@ export const ImageIndexes = styled.View`
   align-self: flex-end;
 
   padding-right: 24px;
-`;
-
-export const ImageIndex = styled.View<ImageIndexProps>`
-  width: 6px;
-  height: 6px;
-
-  background-color: ${({ theme, active }) =>
-    active ? theme.colors.title : theme.colors.shape};
-  border-radius: 3px;
-
-  margin-left: 8px;
 `;
 
 export const CarImageWrapper = styled.View`
--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import {
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    Keyboard,
+} from 'react-native';
 import { NavigationProp, ParamListBase, useNavigation } from '@react-navigation/native';
 
 import { BackButton } from '../../../components/BackButton';
 import { Bullet } from '../../../components/Bullet';
+import { Input } from '../../../components/Input';
+import { Button } from '../../../components/Button';
 
 import {
     Container,
@@ -22,33 +29,59 @@ export function SignUpFirstStep() {
     }
 
     return (
-        <Container>
-            <Header>
-                <BackButton
-                    onPress={handleBack}
-                />
+        <KeyboardAvoidingView
+            behavior='position'
+            enabled
+        >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <Container>
+                    <Header>
+                        <BackButton
+                            onPress={handleBack}
+                        />
 
-                <Steps>
-                    <Bullet active />
-                    <Bullet />
-                </Steps>
-            </Header>
+                        <Steps>
+                            <Bullet active />
+                            <Bullet />
+                        </Steps>
+                    </Header>
 
-            <Title>
-                Crie sua {'\n'}
-                conta
-            </Title>
+                    <Title>
+                        Crie sua {'\n'}
+                        conta
+                    </Title>
 
-            <Subtitle>
-                Faça-se seu cadastro {'\n'}
-                forma rápida e fácil
-            </Subtitle>
+                    <Subtitle>
+                        Faça-se seu cadastro {'\n'}
+                        forma rápida e fácil
+                    </Subtitle>
 
-            <Form>
-                <FormTitle>
-                    1. Dados
-                </FormTitle>
-            </Form>
-        </Container>
+                    <Form>
+                        <FormTitle>
+                            1. Dados
+                        </FormTitle>
+
+                        <Input
+                            iconName='user'
+                            placeholder='Nome'
+                        />
+
+                        <Input
+                            iconName='mail'
+                            placeholder='E-mail'
+                        />
+
+                        <Input
+                            iconName='credit-card'
+                            placeholder='CNH'
+                        />
+                    </Form>
+
+                    <Button
+                        title='Próximo'
+                    />
+                </Container>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     );
 }
